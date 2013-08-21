0a6542391b7cd53cd003efa28b5abb95b8da2069
Ioannis G <jtgftw@gmail.com>
2013-08-20 21:50:01 -0400

    added some documentation

--- system/FileSystem.php
+++ system/FileSystem.php
@@ -42,11 +42,22 @@
 			return $list;
 		}
 
+		/**
+		 * Rename a file or directory
+		 * @param  string $oldname
+		 * @param  string $newname
+		 * @return Boolean
+		 */
 		public function rename($oldname, $newname)
 		{
 			return rename($oldname, $newname);
 		}
 
+		/**
+		 * Delete the given item.
+		 * @param  string $item
+		 * @return Boolean
+		 */
 		public function delete($item)
 		{
 			if ($this->isDirectory($item))
@@ -55,31 +66,61 @@
 				return unlink($item);
 		}
 
+		/**
+		 * Returns the contents of a file.
+		 * @param  string $item
+		 * @return Boolean 
+		 */
 		public function read($item)
 		{
 			return file_get_contents ($item);
 		}
 
+		/**
+		 * Returns true if the given path exists
+		 * @param  string $item
+		 * @return Boolean
+		 */
 		public function exists($item)
 		{
 			return file_exists($item);
 		}
 
+		/**
+		 * Returns true if the given path is an existing directory
+		 * @param  string $item
+		 * @return Boolean
+		 */
 		public function directoryExists($item)
 		{
 			return $this->exists($item) && is_dir($item);
 		}
 
+		/**
+		 * Returns true if the given path is a file
+		 * @param  string  $item
+		 * @return Boolean
+		 */
 		public function isFile($item)
 		{
 			return (bool) is_file($item);
 		}
 
+		/**
+		 * Returns true if the given path is a directory
+		 * @param  string $item
+		 * @return Boolean
+		 */
 		public function isDirectory($item)
 		{
 			return (bool) is_dir($item);
 		}
 
+		/**
+		 * Returns true if the given path is an image
+		 * @param  string  $item
+		 * @return Boolean
+		 */
 		public function isImage($item)
 		{
 			$s = getimagesize($item);
@@ -88,22 +129,43 @@
 		    return in_array($type , array(IMAGETYPE_GIF , IMAGETYPE_JPEG ,IMAGETYPE_PNG , IMAGETYPE_BMP));
 		}
 
+		/**
+		 * Returns true if the given path is a text file
+		 * @param  string  $item
+		 * @return Boolean
+		 */
 		public function isText($item)
 		{			
 			$mime = mime_content_type($item);			
 			return FALSE !== strpos($mime, 'text');
 		}
 
+		/**
+		 * Returns true if the given path is readable
+		 * @param  string  $item
+		 * @return Boolean
+		 */
 		public function canRead($item)
 		{
 			return is_readable($item);
 		}
 
+		/**
+		 * Returns true if the given path is writable
+		 * @param  string  $item
+		 * @return Boolean
+		 */
 		public function canWrite($item)
 		{
 			return is_writeable($item);
 		}
 
+		/**
+		 * Returns the size of the given file, 
+		 * or false if path is a directory
+		 * @param  string $item
+		 * @return mixed
+		 */
 		public function size($item)
 		{
 			if ($this->isDirectory($item))
@@ -112,6 +174,11 @@
 			return filesize ($item);
 		}
 
+		/**
+		 * Returns the last write time of the given item.
+		 * @param  string $item
+		 * @return integer
+		 */
 		public function modtime($item)
 		{
 			return filemtime($item);
